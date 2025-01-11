2f89911f6817f7e221f69ef3e5c168b0fea46bdf
wassim chouiha <chouihawassim@gmail.com>
2025-01-11 18:57:21 +0100

    minor change

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json", 
-    "ngrok-skip-browser-warning": "69420"
+    "ngrok-skip-browser-warning": "69420" // this used only for ignoring a page on ngrok 
   },
 
 });
