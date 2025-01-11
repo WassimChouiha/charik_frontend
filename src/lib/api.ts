df25312c94da930940aa87353fe4707642b13bb4
wassim chouiha <chouihawassim@gmail.com>
2025-01-11 15:31:44 +0100

    changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-// Create an Axios instance
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // Set the base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    "Content-Type": "application/json", // Common headers
+    "Content-Type": "application/json", 
+    "ngrok-skip-browser-warning": "69420"
   },
-  // You can add other default configurations here, e.g., timeout, interceptors, etc.
+
 });
 
 export default api;
